db6848f5c91eefada9074f8203d6b47aefe23a94
Tomáš Rychlik <tomasrychlik@abdoc.net>
2014-04-29 19:02:15 +0200

    Bump to 0.1.5 as a first usable pre-release version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='django-jsonis',
-    version='0.1.4',
+    version='0.1.5',
     description='Django JSON Utils',
     author='Tomas Rychlik',
     author_email='rychlis@rychlis.cz',
