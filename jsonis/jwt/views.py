2e8329ec0dd9eafb6b864ea629dee10f5987997d
Tomáš Rychlik <tomasrychlik@abdoc.net>
2014-06-16 16:52:28 +0200

    WIP: extendable JWTAuthorizationMixin._check_token_data

--- jsonis/jwt/views.py
+++ jsonis/jwt/views.py
@@ -22,7 +22,7 @@ class JWTAuthorizationMixin(JSONApiResponseMixin):
                 if auth_prefix != 'Bearer':
                     return self.render_api_error_response('Not authenticated - Bad authorization header prefix',
                                                           status=401)
-                self.jwt_token_data = parse_token(auth_token)
+                jwt_token_data = parse_token(auth_token)
             except ValueError:
                 return self.render_api_error_response('Not authenticated - Bad authorization header format', status=401)
             except KeyError:
@@ -30,10 +30,18 @@ class JWTAuthorizationMixin(JSONApiResponseMixin):
             except JWTParseError as e:
                 return self.render_api_error_response('Not authenticated - %s' % e, status=401)
 
-            try:
-                self.user = get_user_model().objects.get(pk=self.jwt_token_data['id'])
-            except (TypeError, KeyError):
-                return self.render_api_error_response('Not authenticated - Bad authorization header data', status=401)
-            except get_user_model().DoesNotExist:
-                return self.render_api_error_response('Not authenticated - User not found', status=401)
+            error_response = self._check_token_data(jwt_token_data)
+            if error_response is not None:
+                return error_response
         return super(JWTAuthorizationMixin, self).dispatch(request, *args, **kwargs)
+
+    def _check_token_data(self, jwt_token_data):
+        """Parsed token data checks. Returns a Response if there is an error, None otherwise"""
+        try:
+            self.user = get_user_model().objects.get(pk=self.jwt_token_data['id'])
+        except (TypeError, KeyError):
+            return self.render_api_error_response('Not authenticated - Bad authorization header data', status=401)
+        except get_user_model().DoesNotExist:
+            return self.render_api_error_response('Not authenticated - User not found', status=401)
+        self.jwt_token_data = jwt_token_data
+        return None
